6208b005255ab2248b50653e35437d7288398bbd
Gabriel Nkwa <gnkwa@outlook.com>
2023-06-22 16:25:25 +0100

    new

--- backend/src/routers/userRouter.ts
+++
@@ -1,5 +0,0 @@
-import express, { Request, Response } from 'express'
-import asyncHandler from 'express-async-handler'
-import { UserModel } from '../models/userModel'
-
-export const userRouter = express.Router()
---
+++ backend_sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'rm2ky6he',
+    dataset: 'production'
+  }
+})
