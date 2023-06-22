085f4d70e8df924cf74a4c34ede5a3a06af54658
Gabriel Nkwa <gnkwa@outlook.com>
2023-06-22 19:17:10 +0100

    update

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import { BsInstagram, BsGithub} from 'react-icons/bs'
-import { FaFacebookF}
-
+import React from 'react';
+import { BsInstagram, BsGithub } from 'react-icons/bs';
 
 const SocialMedia = () => {
   return (
-    <div>SocialMedia</div>
-  )
-}
+    <div className="app__social">
+      <div>
+        <BsGithub />
+      </div>
+      <div>
+        <BsInstagram />
+      </div>
+    </div>
+  );
+};
 
-export default SocialMedia
+export default SocialMedia;
--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,8 +2,9 @@
 import React, { useState, useEffect } from 'react';
 import './About.scss';
 import { motion } from 'framer-motion';
-import { images } from '../../constants';
+
 import { urlFor, client } from '../../client';
+import { AppWrap } from '../../wrapper';
 
 const About = () => {
   const [abouts, setAbouts] = useState([]);
@@ -47,4 +48,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default AppWrap(About, 'about');
