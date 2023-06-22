02969ebf5465f2d9b30cd942723f205312bb29a0
Gabriel Nkwa <gnkwa@outlook.com>
2023-06-22 16:25:03 +0100

    new Portfolio

---
+++ frontend_react/src/components/SocialMedia.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BsInstagram, BsGithub} from 'react-icons/bs'
+import { FaFacebookF}
+
+
+const SocialMedia = () => {
+  return (
+    <div>SocialMedia</div>
+  )
+}
+
+export default SocialMedia
